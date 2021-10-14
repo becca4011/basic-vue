7b3e7919ccb63ec8322b801b2f3235279d84328d
becca4011 <becca4011@naver.com>
2021-10-14 18:02:06 +0900

    emit

---
+++ test1/src/components/InputField.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <label for="">Name </label>
+    <input
+      type="text"
+      :value="value"
+      style="padding: 30px; border: 2px solid #42b983"
+      @input="$emit('input', $event.target.value)"
+    />
+    <!-- @input="updateName" -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  //   methods: {
+  //     updateName(e) {
+  //       console.log(e.target.value);
+  //       this.$emit("update-name", e.target.value); // 부모로 e.target.value 보내줌 (이벤트 이름, 보낼 데이터)
+  //     },
+  //   },
+};
+</script>
--- test1/src/views/Home.vue
+++ test1/src/views/Home.vue
@@ -3,19 +3,41 @@
   <div>
     <h1>This is Home Page</h1>
 
+    <form action="">
+      <!-- <InputField :name="name" @update-name="updateName" /> -->
+      <!-- <InputField :name="name" @update-name="name = $event.target.value" /> -->
+      <InputField v-model="name" />
+      <br /><button>Submit</button>
+    </form>
+    {{ name }}
+
     <!-- props -->
-    <Becca title="Home Title" name="Becca" />
+    <!-- <Becca title="Home Title" name="Becca" /> -->
   </div>
 </template>
 
 <!-- 데이터, 메소드 등 배웠던 것이 들어갈 곳 -->
 <script>
-import Becca from "@/components/Becca.vue"; // import를 하면 사용 가능
+// import Becca from "@/components/Becca.vue"; // import를 하면 사용 가능
+import InputField from "@/components/InputField.vue";
 
 export default {
   components: {
-    Becca,
+    InputField,
   },
+  data() {
+    return {
+      name: "",
+    };
+  },
+  // methods: {
+  //   updateName(name) {
+  //     this.name = name; // name에 e.target.value가 넘어옴
+  //   },
+  // },
+  // components: {
+  //   Becca,
+  // },
 };
 </script>
 
